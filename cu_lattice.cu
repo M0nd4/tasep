c2909a676642e088f8aa8fefa29e4c42d149f5cc
Evgeny Toropov <etoropov@andrew.cmu.edu>
2015-04-14 23:41:21 -0400

    dt is sampled

--- cu_lattice.cu
+++ cu_lattice.cu
@@ -2,12 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
 
 #include <thrust/device_vector.h>
 
 #include <curand.h>
 #include <curand_kernel.h>
 
+#include <iomanip>
 
 struct Ribosome {
     double time;
@@ -22,9 +24,28 @@ struct Codon {
 };
 
 
+__global__ 
+void setupKernel ( curandState * state, unsigned long seed )
+{
+    int id = threadIdx.x;
+    curand_init ( seed, id, 0, &state[id] );
+} 
+
+
+__device__ inline static
+double sampleTime (double rate, curandState* globalState)
+{
+    curandState localState = globalState[threadIdx.x];
+    double u = curand_uniform_double( &localState );
+    if (u == 0) u += 0.1; // should almost never happen
+    //if (rate == 0) { return 0; } // this is an error
+    //return u;
+    return -log(u) / rate;  // let rate == 0 throw
+}
+
 //__host__ __device__ inline static
 __global__ static 
-void updatePolysome (Codon* codons, Ribosome* ribosomes, int length)
+void updatePolysome (Codon* codons, Ribosome* ribosomes, int length, curandState* globalState )
 {
     // copy ribosom data from global memory to registers
     int riboId = threadIdx.x;
@@ -38,7 +59,7 @@ void updatePolysome (Codon* codons, Ribosome* ribosomes, int length)
     if (!nextIsFar) return;
 
     // sample the period
-    double dt = 1;
+    double dt = sampleTime(codons[ribo.pos].rate, globalState);
 
     // wait for the following codon to clear (if necessary), then add dt
     double t = max(ribo.time, codons[(ribo.pos + 1 + length) % length].time) + dt;
@@ -63,8 +84,6 @@ void updatePolysome (Codon* codons, Ribosome* ribosomes, int length)
 __global__ static 
 void computePolysome (double* rates, bool* cover, int length, int padding)
 {
-    curandState state;
-    double rand1 = curand_uniform_double(&state);
 }
 */
 
@@ -88,7 +107,6 @@ void runSinglePolysome (const vector<double>& rates, double initRate)
     }
     Codon codon0; codon0.rate = initRate; codon0.time = 0; codon0.occupied = false;
     codonsVector.front() = codon0;
-    //codonsVector.back()  = codon0;
     Codon codon1 = codonsVector[1];
     codon1.occupied = true;
     codonsVector[1] = codon1;
@@ -105,6 +123,12 @@ void runSinglePolysome (const vector<double>& rates, double initRate)
     ribosomesVector[0] = ribo10;
     Ribosome* deviceRibosomes = thrust::raw_pointer_cast( &ribosomesVector[0] );
 
+    // setup seeds
+    curandState* devStates;
+    cudaMalloc ( &devStates, numRibosomes * sizeof(curandState) );
+    setupKernel <<< 1, numRibosomes >>> ( devStates, time(NULL) );    
+        
+    cout << "iteration   codon.occupied   codon.time   ribosome.pos   ribosome.time" << endl;
     for (int it = 0; it != 30; ++it)
     {
         cout << "iteration " << it << ": " << flush;
@@ -113,23 +137,29 @@ void runSinglePolysome (const vector<double>& rates, double initRate)
             Codon codon = codonsVector[i];
             cout << (codon.occupied ? '*' : '.');
         }
-        cout << "  ";
+        cout << "    ";
         for (int i = 0; i != lengthPadded; ++i)
         {
             Codon codon = codonsVector[i];
-            cout << codon.time << "-";
+            cout << setprecision(3) << codon.time << " ";
         }
-        cout << "  ";
+        cout << "    ";
         for (int i = 0; i != numRibosomes; ++i)
         {
             Ribosome ribosome = ribosomesVector[i];
-            cout << ribosome.pos << "-" << ribosome.time << " ";
+            cout << ribosome.pos << " ";
+        }
+        cout << "    ";
+        for (int i = 0; i != numRibosomes; ++i)
+        {
+            Ribosome ribosome = ribosomesVector[i];
+            cout << setprecision(3) << ribosome.time << " ";
         }
         cout << endl;
 
         updatePolysome 
             <<< 1, numRibosomes, 0 >>> 
-            (deviceCodons, deviceRibosomes, lengthPadded);
+            (deviceCodons, deviceRibosomes, lengthPadded, devStates);
 
     }
 
