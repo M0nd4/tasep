421fd71e97a86beaaca1920afcedbff0963958b9
Karanhaar Singh <singhk8900@gmail.com>
2015-04-25 23:04:19 -0400

    added profiling code

--- lattice_ribo.cu
+++ lattice_ribo.cu
@@ -13,6 +13,8 @@
 
 #include <iomanip>
 
+#include <sys/time.h>
+
 // FIXME: for many blocks probably need to create many states
 __global__ static
 void setupRand ( curandState * state, unsigned long seed )
@@ -254,6 +256,17 @@ void printDebug (const In& in, const Out& out, int length)
 void runSinglePolysome (const vector<double>& rates, double epoch, 
                         vector<double>& probs, int verbose)
 {
+    struct timeval tv1, tv2;
+    
+    gettimeofday(&tv1, NULL);
+    cudaFree(0);
+    gettimeofday(&tv2, NULL);
+    printf("CUDA init: Time taken in execution = %f seconds\n",
+           (double) (tv2.tv_usec - tv1.tv_usec) / (double)1000000 +
+           (double) (tv2.tv_sec - tv1.tv_sec));
+           
+    
+    gettimeofday(&tv1, NULL); 
     int length = rates.size();
     cout << "length: " << length << endl;
     
@@ -312,6 +325,11 @@ void runSinglePolysome (const vector<double>& rates, double epoch,
     cudaFree (deviceCodons);
     cudaFree (deviceRibosomes);
     cudaFree (deviceStates);
+    
+    gettimeofday(&tv2, NULL);
+    printf("CUDA - main program: Time taken in execution = %f seconds\n",
+           (double) (tv2.tv_usec - tv1.tv_usec) / (double)1000000 +
+           (double) (tv2.tv_sec - tv1.tv_sec));
 }
 
 
@@ -335,6 +353,17 @@ vector<size_t> orderedLength (const vector< vector<double> >& values) {
 void runMultiplePolysomes (const vector< vector<double> > rates, double epoch,
                            vector< vector<double> >& probs, int verbose)
 {
+
+    struct timeval tv1, tv2;
+    
+    gettimeofday(&tv1, NULL);
+    cudaFree(0);
+    gettimeofday(&tv2, NULL);
+    printf("CUDA init: Time taken in execution = %f seconds\n",
+           (double) (tv2.tv_usec - tv1.tv_usec) / (double)1000000 +
+           (double) (tv2.tv_sec - tv1.tv_sec));
+           
+    gettimeofday(&tv1, NULL);     
     int numRNAs = rates.size();
     probs.resize(numRNAs);
 
@@ -458,4 +487,8 @@ void runMultiplePolysomes (const vector< vector<double> > rates, double epoch,
 
         cudaFree (deviceStates);
     } // split
+    gettimeofday(&tv2, NULL);
+    printf("CUDA main: Time taken in execution = %f seconds\n",
+           (double) (tv2.tv_usec - tv1.tv_usec) / (double)1000000 +
+           (double) (tv2.tv_sec - tv1.tv_sec));
 }
